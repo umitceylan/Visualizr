723b4f0d8f44027b4c9cd7e7218739ba6b0a5168
SunnyLannie <kiqnguye@ucsc.edu>
2016-06-01 16:19:32 -0700

    single company button for boolean value

--- parameters_8000.py
+++ parameters_8000.py
@@ -1,1 +1,1 @@
-password="pbkdf2(1000,20,sha512)$93276787bb4ce053$3077758308a8b20b20d2dde877ebdc8bacbb200a"
+password="pbkdf2(1000,20,sha512)$96edd2fdccc711d6$9efce4757a71ece22ff53b5e32db266ee0940a00"
